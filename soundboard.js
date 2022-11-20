908f8df464b4a1b43d74db75dfed03ffbee14c58
Your Name <jennydinh5@gmail.com>
2022-11-20 15:35:02 -0500

    soundboard

---
+++ soundboard.js
@@ -0,0 +1,18 @@
+var buttonElement = document.getElementById('firstbutton')
+
+buttonElement.addEventListener('click', function () {
+    console.log('I was clicked!')
+})
+
+var playButton = document.getElementById("babyclick");
+
+function babyclick(){
+    sound.play();
+}
+
+var buttonElement = document.getElementById('secondbutton')
+
+buttonElement.addEventListener('click', function () {
+    console.log('I was clicked!')
+})
+
